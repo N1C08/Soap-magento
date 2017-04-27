86aac816badf39393b0d3e949bbc1b83bad79bd7
N1C08 <nico2122@outlook.fr>
2017-04-27 09:49:11 +0200

    update connect

--- sap/app/cnx/cnx.php
+++ sap/app/cnx/cnx.php
@@ -1,11 +1,11 @@
 <?php
 //===== CONNECT customer/invoices=====
-$client = new SoapClient('http://nicolasb.wanalike.fr/magento/index.php/api/soap/?wsdl'); // TODO : change url
-$session = $client->login('SAP', 'cordonelec'); // TODO : change login and pwd if necessary
+$client = new SoapClient('http://YOUR_MAGENTO_HOST/magento/index.php/api/soap/?wsdl'); // TODO : change url
+$session = $client->login('USER_KEY', 'USER_PASS'); // TODO : change login and pwd if necessary
 
 //===== END OF customer/invoices ====
 
 //===== CONNECT =====
-$proxy = new SoapClient('http://nicolasb.wanalike.fr/magento/index.php/api/soap/?wsdl');
-$sessionId = $proxy->login('SAP', 'cordonelec');
+$proxy = new SoapClient('http://YOUR_MAGENTO_HOST/magento/index.php/api/soap/?wsdl');
+$sessionId = $proxy->login('USER_KEY', 'USER_PASS');
 //===== END OF  ====
